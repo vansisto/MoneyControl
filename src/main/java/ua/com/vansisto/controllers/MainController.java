4ce1bb5ec421483d41777259a48b9a76a235680e
Andrii Omelchuk <andrii.omelchuk@openexc.com>
2021-07-03 18:57:02 +0300

    5: Designed initial GUI interface

--- src/main/java/ua/com/vansisto/controllers/MainController.java
+++ src/main/java/ua/com/vansisto/controllers/MainController.java
@@ -1,4 +1,30 @@
 package ua.com.vansisto.controllers;
 
+import javafx.event.ActionEvent;
+
 public class MainController {
+
+    public void clickDoOperationButton(ActionEvent actionEvent) {
+
+    }
+
+    public void clickHistoryButton(ActionEvent actionEvent) {
+
+    }
+
+    public void clickAccountsButton(ActionEvent actionEvent) {
+
+    }
+
+    public void clickUsersButton(ActionEvent actionEvent) {
+
+    }
+
+    public void clickCategoriesButton(ActionEvent actionEvent) {
+
+    }
+
+    public void clickRatesButton(ActionEvent actionEvent) {
+
+    }
 }
