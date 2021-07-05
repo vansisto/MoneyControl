6bd9e59d4a37f362aa9e1d9cf80aca47a055e780
Andrii Omelchuk <andrii.omelchuk@openexc.com>
2021-07-05 22:54:04 +0300

    5: Base design done

--- src/main/java/ua/com/vansisto/controllers/MainController.java
+++ src/main/java/ua/com/vansisto/controllers/MainController.java
@@ -1,8 +1,26 @@
 package ua.com.vansisto.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 
 public class MainController {
+    @FXML
+    public Button do_operation;
+    @FXML
+    public TabPane chart_tab_pane;
+    @FXML
+    public AnchorPane main_split_pane;
+    @FXML
+    public AnchorPane account_statistic;
+
+    @FXML
+    public void initialize() {
+        chart_tab_pane.setPrefHeight(1200);
+        chart_tab_pane.setPrefWidth(700);
+    }
 
     public void clickDoOperationButton(ActionEvent actionEvent) {
 
